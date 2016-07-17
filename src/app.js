238cf7691973b4dc444f1f1e3b386f3535b7fd4d
kenarchersf <archerovi@hotmail.com>
2016-07-17 12:03:31 -0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+//import React, {Component} from 'react';
+//import {render} from 'react-dom';
 
+//class App extends Component {
+//  render() {
+//    return (
+//      <h1>Hello Martin</h1>
+//    );
+//  }
+//}
+
+//render(
+//  <App />,
+//  document.getElementById('app')
+//);
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('app')
+);
