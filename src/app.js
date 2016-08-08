9b5b5cab8b99dd453c2929d043e653a8884772e8
kenarchersf <archerovi@hotmail.com>
2016-08-07 21:54:55 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,63 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import DataSeries from './components/DataSeries';
 
-//class App extends Component {
-//  render() {
-//    return (
-//      <h1>Hello Martin</h1>
-//    );
-//  }
-//}
+const LineChart = React.createClass({
 
-//render(
-//  <App />,
-//  document.getElementById('app')
-//);
+  propTypes: {
+    width:  React.PropTypes.number,
+    height: React.PropTypes.number,
+    data:   React.PropTypes.object.isRequired
+  },
 
-//var React = require('react');
-//var ReactDOM = require('react-dom');
+  getDefaultProps(){
+    return {
+      width:  600,
+      height: 600
+    }
+  },
+
+  render() {
+    let { width, height, data} = this.props;
+
+    let xScale = d3.scale.linear()
+      .domain(0,50)
+      .rangePoints([width, 0]);
+
+    let yScale = d3.scale.linear()
+      .domain([0,50])
+      .range([0, height]);
+   
+
+    return (
+      <svg width={width} height={height}>
+        <DataSeries
+          xScale={xScale}
+          yScale={yScale}
+          data={data}
+          width={width}
+          height={height}
+          />
+      </svg>
+    );
+  }
+
+});
+
+let data = {
+  points: [
+    [ { x: 0, y: 25 }, { x: 50, y: 25 }],
+    [ { x: 5, y: 25 }, { x: 10, y: 20 }],
+    [ { x: 5, y: 25 }, { x: 10, y: 30 }]
+   ]
+};
+
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+    <LineChart
+    data={data}
+    width={600}
+    height={600}
+    />,
   document.getElementById('app')
 );
